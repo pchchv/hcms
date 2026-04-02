7beb72e1a29b3aaf2c56989da38f737f8371ad16
Evgeni Pochechuev <ipchchv@gmail.com>
2026-04-02 07:43:03 +0000

    test: add charset validation for generatePassword

--- database/migrate_test.go
+++ database/migrate_test.go
@@ -16,6 +16,19 @@ func TestGeneratePassword_Length(t *testing.T) {
 	}
 }
 
+func TestGeneratePassword_UsesCharset(t *testing.T) {
+	pw, err := generatePassword(100)
+	if err != nil {
+		t.Fatalf("generatePassword: %v", err)
+	}
+
+	for _, ch := range pw {
+		if !containsRune(passwordCharset, ch) {
+			t.Errorf("password contains invalid character: %q", ch)
+		}
+	}
+}
+
 func containsRune(s string, r rune) bool {
 	for _, c := range s {
 		if c == r {
