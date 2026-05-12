409f33b3e3ceb0daa07c9c0dd14f9002fd87b5d0
Evgeni Pochechuev <ipchchv@gmail.com>
2026-05-12 09:24:18 +0000

    feat(middleware): add responseWriter wrapper to capture status codes

---
+++ middleware/logger.go
@@ -0,0 +1,14 @@
+package middleware
+
+import "net/http"
+
+// responseWriter wraps http.ResponseWriter to capture the status code.
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
+}
