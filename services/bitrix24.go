0ca5cbc7944c7b065076097be0cd044691853e70
Evgeni Pochechuev <ipchchv@gmail.com>
2026-05-13 09:29:11 +0000

    feat(services): define BitrixClient interface for lead integration

---
+++ services/bitrix24.go
@@ -0,0 +1,12 @@
+package services
+
+import (
+	"context"
+
+	"github.com/pchchv/hcms/models"
+)
+
+// BitrixClient defines the interface for sending leads to Bitrix24.
+type BitrixClient interface {
+	SendLead(ctx context.Context, lead models.Lead, webhookURL string) error
+}
