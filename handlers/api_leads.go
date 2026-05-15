098e9eb79ed9da797f0170b644c9077687f50e5d
Evgeni Pochechuev <ipchchv@gmail.com>
2026-05-15 14:37:28 +0000

    feat(handlers): add setCORSHeaders utility for API endpoints

---
+++ handlers/api_leads.go
@@ -0,0 +1,10 @@
+package handlers
+
+import "net/http"
+
+// setCORSHeaders sets permissive CORS headers for API endpoints.
+func setCORSHeaders(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
